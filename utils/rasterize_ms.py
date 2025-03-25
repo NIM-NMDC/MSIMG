fa133c274eb7b0d3fd418b6d746f577135811ff4
WoodFY <zfyfishwood@gmail.com>
2025-03-25 15:57:17 +0800

    Update rasterize ms

--- utils/rasterize_ms.py
+++ utils/rasterize_ms.py
@@ -1,11 +1,12 @@
 import os
-import math
 import numpy as np
 import pyopenms
 import matplotlib.pyplot as plt
 
+from tqdm import tqdm
 
-def rasterize_ms_to_image(ms_file_path, image_path=None, image_size=(500, 500), start_mz=None, end_mz=None, rt_limit=None):
+
+def rasterize_ms_to_image(ms_file_path, image_path=None, image_size=(500, 500), start_mz=None, end_mz=None, start_rt=None, end_rt=None):
     """
     Rasterize mass spectrometry data (MS1) to an image representation.
 
@@ -14,7 +15,8 @@ def rasterize_ms_to_image(ms_file_path, image_path=None, image_size=(500, 500), 
     :param image_size: Tuple representing the size of the output image (default is (1000, 1000)).
     :param start_mz: Optional float, the lower bound of m/z range for imaging.
     :param end_mz: Optional float, the upper bound of m/z range for imaging.
-    :param rt_limit: Optional float, if provided, spectra with rt > rt_limit will be skipped.
+    :param start_rt: Optional float, the lower bound of RT range for imaging.
+    :param end_rt: Optional float, the upper bound of RT range for imaging.
     :return: None
     """
     # Load the mass spectrometry file.
@@ -44,13 +46,12 @@ def rasterize_ms_to_image(ms_file_path, image_path=None, image_size=(500, 500), 
         if spectrum.getMSLevel() == 1:
             rt = spectrum.getRT()
 
-            if rt_limit and rt > rt_limit:
+            if start_rt and rt < start_rt:
+                continue
+            if end_rt and rt > end_rt:
                 continue
 
             mz_array, intensity_array = spectrum.get_peaks()
-
-            if len(mz_array) == 0:
-                continue
 
             # Update the min and max values
             min_rt = min(min_rt, rt)
@@ -69,31 +70,26 @@ def rasterize_ms_to_image(ms_file_path, image_path=None, image_size=(500, 500), 
 
     if start_mz:
         min_mz = max(min_mz, start_mz)
-
     if end_mz:
         max_mz = min(max_mz, end_mz)
 
     image = np.zeros(image_size)
-    # Set the retention time and m/z ranges.
-    rt_scale = np.linspace(min_rt, max_rt, image_size[0])  # y-axis (Retention Time)
+    # Set the m/z ranges and retention time.
     mz_scale = np.linspace(min_mz, max_mz, image_size[1])  # x-axis (M/Z)
+    rt_scale = np.linspace(min_rt, max_rt, image_size[0])  # y-axis (Retention Time)
 
     for i in tqdm(range(len(rt_list))):
         rt = rt_list[i]
         mz_array = mz_arrays[i]
         intensity_array = intensity_arrays[i]
 
-        # Skip if there are no peaks with non-zero intensity.
-        if len(intensity_array) == 0:
-            continue
-
         valid_idx = (mz_array >= min_mz) & (mz_array <= max_mz) & (intensity_array > 0)
         mz_array = mz_array[valid_idx]
         intensity_array = intensity_array[valid_idx]
 
         # Find the corresponding retention time and m/z indices.
-        y_idx = np.argmin(np.abs(rt_scale - rt))
         x_idx = np.digitize(mz_array, mz_scale) - 1
+        y_idx = np.argmin(np.abs(rt_scale - rt))
 
         # Map the intensity values to the image.
         for j in range(len(mz_array)):
@@ -109,15 +105,13 @@ def rasterize_ms_to_image(ms_file_path, image_path=None, image_size=(500, 500), 
     if image_path:
         # Save image with no extra padding.
         plt.savefig(image_path, bbox_inches='tight', pad_inches=0, transparent=True)
-    plt.show()
+    # plt.show()
     plt.close()
 
 
 if __name__ == '__main__':
 
-    from tqdm import tqdm
-
-    ms_files_dir = r'E:\msdata\ST000923\HMP2_C8-pos\C8p_rawData\test'
+    ms_files_dir = r'E:\msdata\ST003161_Rawfiles\SOURCE_Israel_Stool_2022_mzml'
     ms_files_path = []
     if os.path.exists(ms_files_dir):
         for root, dirs, files in os.walk(ms_files_dir):
@@ -127,5 +121,8 @@ if __name__ == '__main__':
                     ms_files_path.append(file_path)
 
     for ms_file_path in ms_files_path:
-        image_path = os.path.splitext(ms_file_path)[0] + '.png'
-        rasterize_ms_to_image(ms_file_path, image_path=image_path, image_size=(500, 500), start_mz=300, end_mz=600, rt_limit=600)
+        images_dir = os.path.join(ms_files_dir, 'images')
+        os.makedirs(images_dir, exist_ok=True)
+        image_path = os.path.join(images_dir, os.path.splitext(os.path.basename(ms_file_path))[0] + '.png')
+        # image_path = os.path.splitext(ms_file_path)[0] + '.png'
+        rasterize_ms_to_image(ms_file_path, image_path=image_path, image_size=(500, 500))
