6cea9463ccbd39aaf5b7e337e399af70ce66c43e
WoodFY <zfyfishwood@gmail.com>
2025-04-05 17:14:08 +0800

    Update prepare datasets

--- datasets/prepare_datasets.py
+++ datasets/prepare_datasets.py
@@ -1,3 +1,5 @@
+import os
+
 from utils.file_split import split_dataset_files_by_label_stratified
 from datasets.transforms import build_base_transform, build_dynamic_transform
 from datasets.datasets import MS2DImgDataset
@@ -8,7 +10,7 @@ def prepare_ibd_ms_img_dataset(exp_args, label_mapping):
     Prepare the IBD mass spectrometry image dataset.
     """
     train_set, valid_set, test_set = split_dataset_files_by_label_stratified(
-        root_dir=exp_args['ms_img_dir'],
+        root_dir=exp_args['dataset_dir'],
         train_size=0.8,
         test_size=0.1,
         random_seed=exp_args['random_seed']
@@ -33,9 +35,30 @@ def prepare_ibd_ms_img_dataset(exp_args, label_mapping):
     valid_transform = build_base_transform(config=transform_config)
     test_transform = build_base_transform(config=transform_config)
 
-    train_dataset = MS2DImgDataset(file_paths=train_set, label_mapping=label_mapping, transform=train_transform)
-    valid_dataset = MS2DImgDataset(file_paths=valid_set, label_mapping=label_mapping, transform=valid_transform)
-    test_dataset = MS2DImgDataset(file_paths=test_set, label_mapping=label_mapping, transform=test_transform)
+    def get_label_from_path(file_path):
+        """
+        Extract the label from the file path.
+
+        - Dataset structure:
+            - IBD_2D
+                - full_mz
+                    - CD
+                        - ST000923-C8-pos
+                            - 123.png
+                    - HC
+                        - ST000923-C8-pos
+                            - 456.png
+                    - UC
+                        - ST000923-C8-pos
+                            - 789.png
+        """
+        label = os.path.basename(os.path.dirname(os.path.dirname(file_path)))
+        return label
+
+
+    train_dataset = MS2DImgDataset(file_paths=train_set, label_mapping=label_mapping, get_label_function=get_label_from_path, transform=train_transform)
+    valid_dataset = MS2DImgDataset(file_paths=valid_set, label_mapping=label_mapping, get_label_function=get_label_from_path, transform=valid_transform)
+    test_dataset = MS2DImgDataset(file_paths=test_set, label_mapping=label_mapping, get_label_function=get_label_from_path, transform=test_transform)
 
     return train_dataset, valid_dataset, test_dataset
 
