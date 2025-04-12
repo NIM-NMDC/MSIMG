1dd8d3e10079a63109416fc0c67b02a087915e72
WoodFY <zfyfishwood@gmail.com>
2025-04-12 18:53:53 +0800

    Update datasets - domain dataset

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -5,34 +5,84 @@ import os
 from PIL import Image
 
 
-class MS2DImgDataset(Dataset):
+class MS2DIMGDataset(Dataset):
 
-    def __init__(self, file_paths, label_mapping, get_label_function=None, transform=None):
+    def __init__(self, file_paths, label_mapping, transform=None):
         self.file_paths = file_paths
         self.label_mapping = label_mapping
         self.transform = transform
 
-        self.img_samples = []
-        for file_path in file_paths:
-            if get_label_function:
-                label = get_label_function(file_path)
-            else:
-                raise ValueError('get_label_function is required.')
-            self.img_samples.append(
+        self.samples = []
+        for class_name, file_path in file_paths:
+            self.samples.append(
                 {
                     'file_path': file_path,
-                    'label': self.label_mapping[label]
+                    'label': self.label_mapping[class_name]
                 }
             )
 
-    def __len__(self):
-        return len(self.img_samples)
-
     def __getitem__(self, idx):
-        img_sample = self.img_samples[idx]
-        img = Image.open(img_sample['file_path']).convert('L')
-
+        file_path, label = self.samples[idx]['file_path'], self.samples[idx]['label']
+        img = Image.open(file_path).convert('L')
         if self.transform:
             img = self.transform(img)
+        return img, label
 
-        return img, img_sample['label']
+    def __len__(self):
+        return len(self.samples)
+
+
+class MS2DIMGDomainDataset(Dataset):
+    """
+    MS2D Image Multiple Domain Dataset.
+
+    Dataset Structure:\n
+    dataset_dir/
+        Domain A/
+            - Class A/ [IMGs]
+            - Class B/ [IMGs]
+        Domain B/
+            - Class A/ [IMGs]
+            - Class B/ [IMGs]
+
+    :param file_paths: List of file paths to the images (e.g., [(domain_name, class_name, file_path), ...]).
+    :param transform: Transform to be applied to the images.
+    :param task: Task type. If 'domain_discrepancy_validation', use the domain as the label.
+    """
+    def __init__(self, file_paths, label_mapping, transform=None, task=None):
+        self.file_paths = file_paths
+        self.label_mapping = label_mapping
+        self.transform = transform
+        self.task = task
+
+        self.samples = []
+        self._load_samples()
+
+    def _get_label(self, domain_name, class_name):
+        if self.task == 'domain_discrepancy_validation':
+            return domain_name
+        elif self.task == 'classification':
+            return class_name
+        else:
+            raise ValueError(f"Unknown task: {self.task}")
+
+    def _load_samples(self):
+        for domain_name, class_name, file_path in self.file_paths:
+            self.samples.append(
+                {
+                    'file_path': file_path,
+                    'label': self.label_mapping[self._get_label(domain_name=domain_name, class_name=class_name)]
+                }
+            )
+
+    def __getitem__(self, idx):
+        file_path, label = self.samples[idx]['file_path'], self.samples[idx]['label']
+        img = Image.open(file_path).convert('L')
+        if self.transform:
+            img = self.transform(img)
+        return img, label
+
+    def __len__(self):
+        return len(self.samples)
+
+
