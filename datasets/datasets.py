043b7ff23c0739fc446297ac37a6d720b97774fa
WoodFY <zfyfishwood@gmail.com>
2025-04-13 16:35:55 +0800

    Update datasets

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -1,40 +1,43 @@
 import torch
 from torch.utils.data import Dataset
-
-import os
-from PIL import Image
+import numpy as np
 
 
 class MS2DIMGDataset(Dataset):
+    """
+    MS 2D Image Dataset.
 
-    def __init__(self, file_paths, label_mapping, transform=None):
-        self.file_paths = file_paths
+    Dataset Structure:\n
+    dataset_dir/
+        - Class A/ [IMGs]
+        - Class B/ [IMGs]
+
+    :param dataset: List of tuples containing (class_name, file_path).
+    :param label_mapping: Mapping from class names to labels.
+    :param transform: Transform to be applied to the images.
+    """
+    def __init__(self, dataset, label_mapping, transform=None):
+        self.dataset = dataset
         self.label_mapping = label_mapping
         self.transform = transform
 
-        self.samples = []
-        for class_name, file_path in file_paths:
-            self.samples.append(
-                {
-                    'file_path': file_path,
-                    'label': self.label_mapping[class_name]
-                }
-            )
-
     def __getitem__(self, idx):
-        file_path, label = self.samples[idx]['file_path'], self.samples[idx]['label']
-        img = Image.open(file_path).convert('L')
+        sample = np.load(self.dataset[idx]['file_path'])
+        patches = sample['patches']
+        positions = sample['positions']
+        padding_mask = sample['padding_mask']
+        label = self.label_mapping[self.dataset[idx]['class_name']]
         if self.transform:
-            img = self.transform(img)
-        return img, label
+            patches = self.transform(patches)
+        return patches, positions, padding_mask, label
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.dataset)
 
 
 class MS2DIMGDomainDataset(Dataset):
     """
-    MS2D Image Multiple Domain Dataset.
+    MS 2D Image Multiple Domain Dataset.
 
     Dataset Structure:\n
     dataset_dir/
@@ -45,44 +48,40 @@ class MS2DIMGDomainDataset(Dataset):
             - Class A/ [IMGs]
             - Class B/ [IMGs]
 
-    :param file_paths: List of file paths to the images (e.g., [(domain_name, class_name, file_path), ...]).
+    :param dataset: List of tuples containing (domain_name, class_name, file_path).
+    :param label_mapping: Mapping from class names to labels.
     :param transform: Transform to be applied to the images.
     :param task: Task type. If 'domain_discrepancy_validation', use the domain as the label.
     """
-    def __init__(self, file_paths, label_mapping, transform=None, task=None):
-        self.file_paths = file_paths
+    def __init__(self, dataset, label_mapping, transform=None, task=None):
+        self.dataset = dataset
         self.label_mapping = label_mapping
         self.transform = transform
         self.task = task
 
-        self.samples = []
-        self._load_samples()
-
-    def _get_label(self, domain_name, class_name):
+    def _get_label(self, domain_name, class_name, label_mapping):
         if self.task == 'domain_discrepancy_validation':
-            return domain_name
+            return label_mapping[domain_name]
         elif self.task == 'classification':
-            return class_name
+            return label_mapping[class_name]
         else:
             raise ValueError(f"Unknown task: {self.task}")
 
-    def _load_samples(self):
-        for domain_name, class_name, file_path in self.file_paths:
-            self.samples.append(
-                {
-                    'file_path': file_path,
-                    'label': self.label_mapping[self._get_label(domain_name=domain_name, class_name=class_name)]
-                }
-            )
-
     def __getitem__(self, idx):
-        file_path, label = self.samples[idx]['file_path'], self.samples[idx]['label']
-        img = Image.open(file_path).convert('L')
+        sample = np.load(self.dataset[idx]['file_path'])
+        patches = sample['patches']
+        positions = sample['positions']
+        padding_mask = sample['padding_mask']
+        label = self._get_label(
+            domain_name=self.dataset[idx]['domain_name'],
+            class_name=self.dataset[idx]['class_name'],
+            label_mapping=self.label_mapping
+        )
         if self.transform:
-            img = self.transform(img)
-        return img, label
+            patches = self.transform(patches)
+        return patches, positions, padding_mask, label
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.dataset)
 
 
