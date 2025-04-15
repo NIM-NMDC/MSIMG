a8595d17e61cf74371ae872a29e85845882c4e9f
WoodFY <zfyfishwood@gmail.com>
2025-04-15 15:16:19 +0800

    Update exp classification

--- exps/exp_classification.py
+++ exps/exp_classification.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 
 import os
 import numpy as np
@@ -17,40 +17,41 @@ from callbacks.early_stopping import EarlyStopping
 from utils.train_utils import train, test
 
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1, 2, 3, 4, 5, 6, 7"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1, 2, 3"
 
 
-def create_dataset(dataset, label_mapping, transform=None):
+def create_dataset(dataset, label_mapping, transform=None, preload=False):
     return MS2DIMGDataset(
         dataset=dataset,
         label_mapping=label_mapping,
-        transform=transform
+        transform=transform,
+        preload=preload
     )
 
 
 def exp(args):
 
     train_set, valid_set, test_set = split_dataset_files_by_class_stratified(
-        root_dir=args.dataset_dir,
+        dataset_dir=args.dataset_dir,
         train_size=0.8,
         test_size=0.1,
         random_seed=args.random_seed
     )
 
     train_loader = DataLoader(
-        create_dataset(train_set, args.label_mapping),
+        create_dataset(dataset=train_set, label_mapping=args.label_mapping, preload=args.preload),
         batch_size=args.batch_size,
         shuffle=True,
         num_workers=args.num_workers
     )
     valid_loader = DataLoader(
-        create_dataset(valid_set, args.label_mapping),
+        create_dataset(dataset=valid_set, label_mapping=args.label_mapping, preload=args.preload),
         batch_size=args.batch_size,
         shuffle=False,
         num_workers=args.num_workers
     )
     test_loader = DataLoader(
-        create_dataset(valid_set, args.label_mapping),
+        create_dataset(dataset=test_set, label_mapping=args.label_mapping, preload=args.preload),
         batch_size=args.batch_size,
         shuffle=False,
         num_workers=args.num_workers
@@ -69,7 +70,7 @@ def exp(args):
         model_name=args.model_name.lower(),
         num_classes=args.num_classes,
         in_channels=args.top_k,
-        # pretrained=args.pretrained
+        pretrained=args.pretrained
     )
 
     if args.use_multi_gpu and torch.cuda.device_count() > 1:
@@ -142,6 +143,7 @@ def main():
     parser.add_argument('--save_dir', type=str, default='checkpoints', help='Directory to save checkpoints')
     parser.add_argument('--model_name', type=str, default='ResNet50', help='Model name')
     parser.add_argument('--dataset_name', type=str, help='Dataset to use')
+    parser.add_argument('--label_maps', nargs='+', help='List of label maps to use (e.g. HC=0 CD=1 UC=2)')
 
     parser.add_argument('--top_k', type=int, default=512, help='Number of patches to be selected')
     parser.add_argument('--patch_width', type=int, default=224, help='Width of the patches to be extracted')
@@ -153,7 +155,9 @@ def main():
     parser.add_argument('--batch_size', type=int, default=64, help='Batch size')
     parser.add_argument('--epochs', type=int, default=128, help='Number of epochs')
     parser.add_argument('--device', type=str, default=None, help='Device to use')
+    parser.add_argument('--preload', action='store_true', help='Preload dataset into memory')
     parser.add_argument('--num_workers', type=int, default=32, help='Number of workers for DataLoader')
+    parser.add_argument('--pretrained', action='store_true', help='Use pretrained model')
     parser.add_argument('--use_multi_gpu', action='store_true', help='Use multiple GPUs')
     parser.add_argument('--use_augmentation', action='store_true', help='Use augmentation')
     parser.add_argument('--use_early_stopping', action='store_true', help='Use early stopping')
@@ -169,21 +173,46 @@ def main():
         if args.patience is None:
             args.patience = 10
 
+    if args.preload:
+        args.num_workers = 0  # Set to 0 to avoid issues with DataLoader
+
     # Set save directory
     save_dir = os.path.join(args.root_dir, args.save_dir)
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     args.save_dir = save_dir
 
+    dataset_parent_dir = f"datasets/IBD_2D/top_{args.top_k}_patch_{args.patch_width}x{args.patch_height}_overlap_{args.overlap_col}x{args.overlap_row}_bin_size_{args.bin_size}"
     dataset_dict = {
-        'ST000923-C8-pos': f'datasets/IBD_2D/top_{args.top_k}_patch_{args.patch_width}x{args.patch_height}_overlap_{args.overlap_col}x{args.overlap_row}_bin_size_{args.bin_size}/ST000923-C8-pos',
+        'ST000923-C8-pos': f"{dataset_parent_dir}/ST000923-C8-pos",
+        'ST000923-C18-neg': f"{dataset_parent_dir}/ST000923-C18-neg",
+        'ST000923-HILIC-pos': f"{dataset_parent_dir}/ST000923-HILIC-pos",
+        'ST000923-HILIC-neg': f"{dataset_parent_dir}/ST000923-HILIC-neg",
+        'ST001000-C8-pos': f"{dataset_parent_dir}/ST001000-C8-pos",
+        'ST001000-C18-neg': f"{dataset_parent_dir}/ST001000-C18-neg",
+        'ST001000-HILIC-pos': f"{dataset_parent_dir}/ST001000-HILIC-pos",
+        'ST0001000-HILIC-neg': f"{dataset_parent_dir}/ST001000-HILIC-neg",
+        'ST003161': f"{dataset_parent_dir}/ST003161",
+        'ST003313': f"{dataset_parent_dir}/ST003313",
+        'PXD10371': f"{dataset_parent_dir}/PXD10371",
+        'MSV000089237': f"{dataset_parent_dir}/MSV000089237",
     }
     dataset_dir = os.path.join(args.root_dir, dataset_dict[args.dataset_name])
     if not os.path.exists(dataset_dir):
         raise FileNotFoundError(f"Dataset directory {dataset_dir} does not exist.")
     args.dataset_dir = dataset_dir
 
-    label_mapping = {'nonIBD': 0, 'CD': 1, 'UC': 2}
+    label_mapping = {}
+    if args.label_maps:
+        for pair in args.label_maps:
+            label, value = pair.split('=')
+            if value.isdigit():
+                label_mapping[label] = int(value)
+            else:
+                raise ValueError(f"Invalid label mapping: {pair}. Value must be an integer.")
+    else:
+        label_mapping = {'HC': 0, 'CD': 1, 'UC': 2}
+
     args.label_mapping = label_mapping
     args.num_classes = len(label_mapping)
 
